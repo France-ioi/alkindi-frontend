35c9292cae4a16552d578e855d67a6e3f4813918
Sebastien Carlier <s.carlier@epixode.fr>
2015-11-16 14:30:26 +0000

    initial import

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+// very simple compiler
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var chmod = require('gulp-chmod');
+var eol = require('gulp-eol');
+var eslint = require('gulp-eslint');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var watchify = require('watchify');
+var assign = require('lodash.assign');
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
+
+function buildScript (options) {
+    var browserifyOpts = {
+        entries: [options.entry],
+        debug: true,
+        transform: [reactify]
+    };
+    if (options.watch)
+        browserifyOpts = assign({}, watchify.args, browserifyOpts);
+    var bundler = browserify(browserifyOpts);
+    if (options.watch)
+        bundler = watchify(bundler);
+
+    function rebundle () {
+        var p = bundler.bundle()
+            .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+            .pipe(source(options.output))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({loadMaps: true}));
+        if (options.uglify)
+            p = p.pipe(uglify());
+        return p
+            .pipe(sourcemaps.write("."))
+            .pipe(chmod(644))
+            .pipe(eol("\n"))
+            .pipe(gulp.dest("dist"));
+    }
+
+    bundler.on('log', gutil.log);
+    bundler.on('update', rebundle);
+    return rebundle();
+}
+
+function watched(opts) {
+    return assign({}, opts, {watch: true});
+}
+
+function uglified(opts) {
+    var output = opts.output.replace(/.js$/, '.min.js');
+    return assign({}, opts, {uglify: true, output: output});
+}
+
+var mainScriptOpts = {
+    entry: 'src/app.js',
+    output: 'app.js',
+    watch: false,
+    uglify: false
+};
+
+gulp.task('build', [], function () {
+    buildScript(mainScriptOpts);
+});
+
+gulp.task('build_min', [], function () {
+    buildScript(uglified(mainScriptOpts))
+});
+
+gulp.task('watch', [], function () {
+    buildScript(watched(mainScriptOpts));
+});
+
+gulp.task('lint', function() {
+  return gulp.src('./src/**/*.js')
+    .pipe(eslint({
+        extends: "eslint:recommended",
+        ecmaFeatures: {
+            jsx: true
+        },
+        plugins: [
+            "react"
+        ],
+        envs: [
+            'browser',
+            'commonjs'
+        ]
+    }))
+    .pipe(eslint.format());
+});
+
+gulp.task('default', ['build']);
---
+++ src/app.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// IE8 compatibility:
+require('es5-shim');
+require('es5-sham-ie8');
+// Use el.getAttribute('data-value') rather than el.dataset.value.
+
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var classnames = require('classnames');
+
+var Home = React.createClass({
+  getInitialState: function () {
+    return {selected: 0};
+  },
+  getDefaultProps: function () {
+    return {title: 'bar'};
+  },
+  propTypes: function () {
+    return {title: React.PropTypes.string};
+  },
+  render: function () {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        {this.renderItems()}
+      </div>
+    );
+  },
+  renderItems:  function () {
+    var iSelected = this.state.selected, i, j, items = [];
+    for (i = 0, j = 10; i < j; i++) {
+      var classes = classnames({
+        selected: i === iSelected
+      });
+      items.push(<li key={i} data-value={i} className={classes} onClick={this.onClick}>item {i}</li>);
+    }
+    return (<ul>{items}</ul>);
+  },
+  onClick: function (event) {
+    var value = event.currentTarget.getAttribute('data-value');
+    this.setState({selected: parseInt(value)});
+  }
+});
+
+var mainElement = document.getElementById('main');
+ReactDOM.render(
+  <div>
+    <Home title="left"/>
+    <Home title="right"/>
+  </div>, mainElement);
