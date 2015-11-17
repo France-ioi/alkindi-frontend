ef17e2e4de1426bc072708936ae42cd98f5d09a0
Melanie Storup <m.storup@epixode.fr>
2015-11-17 22:15:18 +0100

    style and html hints

--- src/tools/hints.js
+++ src/tools/hints.js
@@ -25,31 +25,35 @@ module.exports = React.createClass({
           d = reversePermutation[i], showD = this.state.revealed[d];
       console.log(i, c, showC, d, showD);
       forwardMapping.push(
-        <div key={i}>
-          <span>{alphabet[i]}</span>
-          <span>{showD ? alphabet[d] : ' '}</span>
+        <div key={i} className="char-pairs">
+          <span className="char-base cipher">{alphabet[i]}</span>
+          <span className="char-subs">{showD ? alphabet[d] : ' '}</span>
         </div>
       );
       backwardMapping.push(
-        <div key={i}>
-          <span>{alphabet[i]}</span>
-          <span>{showC ? alphabet[c] : ' '}</span>
+        <div key={i} className="char-pairs">
+          <span className="char-base">{alphabet[i]}</span>
+          <span className="char-subs cipher">{showC ? alphabet[c] : ' '}</span>
         </div>
       );
     }
     return (
       <div className="hints">
-        <p>...</p>
-        <p>...</p>
+        <p>Demandez des indices pour vous aider (les points utilisés seront retirés de votre score)</p>
+        <p id="hint-score">Points restants : 250/300</p>
         <hr/>
         <div className="forwardMapping">
-          {forwardMapping}
-          <p>...</p>
+          <div className="clearfix">
+            {forwardMapping}
+          </div>
+          <p>Pour 10 points, cliquez sur une case pour connaître la lettre associée un symbole.</p>
         </div>
         <hr/>
-        <div className="backwardMapping">
-          {backwardMapping}
-          <p>...</p>
+        <div className="backwardMapping clearfix">
+          <div className="clearfix">
+            {backwardMapping}
+          </div>
+          <p>Pour 15 points, cliquez sur une case pour connaître le symbole associé à une lettre.</p>
         </div>
       </div>);
   }
