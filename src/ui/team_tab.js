88ebdcb67c0d56fd06d85d38510a1b41b9570456
Sebastien Carlier <s.carlier@epixode.fr>
2015-12-31 14:25:49 +0000

    fix team_tab

--- src/ui/team_tab.js
+++ src/ui/team_tab.js
@@ -5,20 +5,30 @@ import {PureComponent} from '../misc';
 import * as actions from '../actions';
 
 const TeamTab = PureComponent(self => {
-  function onCreateTeam () {
-    // TODO: interact with server
-    // for now: dispatch a SET_TEAM event
-  }
-  function onShowJoinTeam () {
-    self.setState({joinTeam: true});
-  }
-  function onJoinTeam () {
-    // TODO: interact with server
-    // self.refs.teamCode
-    // for now: dispatch a SET_TEAM event
-  }
-  function isOpenChanged () {
-    // TODO
+  const onIsOpenChanged = function (event) {
+    console.log('isOpen', event.currentTarget, event.currentTarget.value);
+    self.setState({
+      isOpen: event.currentTarget.value === 'true'
+    });
+  };
+  const onLeaveTeam = function () {
+    console.log('leaveTeam');
+  };
+  const onUpdateTeam = function () {
+    console.log('updateTeam', {is_open: self.state.isOpen});
+  };
+  const renderMember = function (member) {
+    const flags = [];
+    if (self.props.team.creator.id === member.user.id)
+      flags.push('créateur');
+    if (member.is_selected)
+      flags.push('sélectionné');
+    return (
+      <tr key={member.user.id}>
+        <td>{member.user.username}</td>
+        <td>{flags.join(', ')}</td>
+        <td>{new Date(member.joined_at).toLocaleString()}</td>
+      </tr>);
   }
   self.render = function () {
     const {user, team} = self.props;
@@ -28,27 +38,6 @@ const TeamTab = PureComponent(self => {
         <div key='code'>
           <p>Code d'accès de l'équipe : <strong>{team.code}</strong></p>
         </div>);
-      body.push(
-        <div key='settings'>
-          <p>Vous pouvez modifier les réglages de votre équipe :</p>
-          <p><input type="radio" name="team-open" id="team-open" checked={team.isOpen} onChange={isOpenChanged} /> <label htmlFor="team-open">Permettre à d'autres personnes de rejoindre l'équipe</label></p>
-          <p><input type="radio" name="team-open" id="team-closed" checked={!team.isOpen} onChange={isOpenChanged} /> <label htmlFor="team-closed">Bloquer les inscriptions à l'équipe</label></p>
-          <button type="submit" className="submit">Enregistrer les modifications</button>
-        </div>);
-    }
-    const renderMember = function (member) {
-      const flags = [];
-      if (team.creator.id === member.user.id)
-        flags.push('créateur');
-      if (member.isSelected)
-        flags.push('sélectionné');
-      return (
-        <tr key={member.user.id}>
-          <td>{member.user.fullname}</td>
-          <td>{member.user.email}</td>
-          <td>{flags.join(', ')}</td>
-          <td>{member.joinDate}</td>
-        </tr>);
     }
     body.push(
       <div key='members'>
@@ -64,22 +53,34 @@ const TeamTab = PureComponent(self => {
           </tbody>
         </table>
       </div>);
+    if (team.creator.id === user.id) {
+      body.push(
+        <div key='settings'>
+          <p>Vous pouvez modifier les réglages de votre équipe :</p>
+          <p>
+            <input type="radio" name="team-open" value="true"  id="team-open" checked={self.state.isOpen} onChange={onIsOpenChanged} />
+             <label htmlFor="team-open">Permettre à d'autres personnes de rejoindre l'équipe</label>
+          </p>
+          <p>
+            <input type="radio" name="team-open" value="false" id="team-closed" checked={!self.state.isOpen} onChange={onIsOpenChanged} />
+             <label htmlFor="team-closed">Bloquer les inscriptions à l'équipe</label>
+           </p>
+          <button type="submit" className="submit" onClick={onUpdateTeam}>Enregistrer les modifications</button>
+        </div>);
+    }
     if (!self.props.haveQuestion) {
-      if (team.creator.id === user.id) {
-        if (team.members.length === 1) {
-          body.push(<p key='dissolve'><button type="button" className="submit">Dissoudre l'équipe</button></p>);
-        } else {
-          body.push(<p key='dissolve'>D'autres membres ont rejoint l'équipe, vous ne pouvez plus la dissoudre.</p>);
-        }
-      } else {
-        body.push(<p key='leave'><button type="button" className="submit">Quitter l'équipe</button></p>);
-      }
+      body.push(
+        <p key='leave'>
+          <button type="button" className="submit" onClick={onLeaveTeam}>Quitter l'équipe</button>
+        </p>
+      );
     }
     return (<div className="wrapper">{body}</div>);
   };
 }, self => {
   return {
-    joinTeam: false
+    joinTeam: false,
+    isOpen: self.props.team.is_open
   };
 });
 
