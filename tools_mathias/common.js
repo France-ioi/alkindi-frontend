444ff31b019efdcd8080a0c9092736adf7323895
Sebastien Carlier <s.carlier@epixode.fr>
2016-01-06 23:59:18 +0000

    remove uses of this

--- tools_mathias/common.js
+++ tools_mathias/common.js
@@ -38,7 +38,7 @@ var common = {
          for (var col = 0; col < nbCols; col++) {
             var cell = gridCells[row][col];
             if (cell.q != 'unknown') {
-               letterQualifiers[cell.l] = this.maxQualifier[letterQualifiers[cell.l]][cell.q];
+               letterQualifiers[cell.l] = common.maxQualifier[letterQualifiers[cell.l]][cell.q];
             }
          }
       }
@@ -67,7 +67,7 @@ var common = {
          html += "<div class='variable-entree variable-informations'>";
          html += "<span>Variables d'entrée :</span>";
          for (var iVar = 0; iVar < variables.input.length; iVar++) {
-            html += this.renderVariable(variables.input[iVar]);
+            html += common.renderVariable(variables.input[iVar]);
          }
          html += "</div>";
       }
@@ -75,7 +75,7 @@ var common = {
          html += "<div class='variable-sortie variable-informations'>";
          html += "<span>Variables de sortie :</span>";
          for (var iVar = 0; iVar < variables.output.length; iVar++) {
-            html += this.renderVariable(variables.output[iVar]);
+            html += common.renderVariable(variables.output[iVar]);
          }
          html += "</div>";
       }
@@ -143,7 +143,7 @@ var common = {
    },
 
    getWrappingInfos: function(text, maxWidth, alphabet) {
-      var letterRanks = this.getLetterRanks(alphabet);
+      var letterRanks = common.getLetterRanks(alphabet);
       var lineStartCols = [0];
       var col = 0;
       var nextCut = 0;
@@ -167,7 +167,7 @@ var common = {
    },
 
    testWrapping: function(text, maxWidth, alphabet) {
-      var lineStartCols = this.getWrappingInfos(text, maxWidth, alphabet);
+      var lineStartCols = common.getWrappingInfos(text, maxWidth, alphabet);
       for (var iLine = 0; iLine < lineStartCols.length - 1; iLine++) {
          var startCol = lineStartCols[iLine];
          var endCol = lineStartCols[iLine + 1];
