fe267376feb64db98e25c85b52d5285d2b9de7cd
Sebastien Carlier <s.carlier@epixode.fr>
2016-01-08 09:04:13 +0000

    mode editable bigram_frequency_analysis + nettoyage

--- tools_mathias/tools-react/tools/bigram_frequency_analysis.js
+++ tools_mathias/tools-react/tools/bigram_frequency_analysis.js
@@ -6,7 +6,7 @@ import {OkCancel} from '../ui/ok_cancel';
 import * as Python from '../python';
 import {getCellLetter, getQualifierClass} from '../tools';
 import {getTextBigrams, getMostFrequentBigrams, getBigramSubstPair,
-        countSubstitutionConflicts, identitySubstPair, applySubstitutionEdits} from '../bigram_utils';
+        countSubstitutionConflicts, applySubstitutionEdits} from '../bigram_utils';
 import EditPairDialog from '../ui/edit_pair_dialog';
 const nullSubstPair = {dst: [{q: "unknown"}, {q: "unknown"}]};
 
@@ -68,33 +68,39 @@ export const Component = PureComponent(self => {
    };
 
    const renderInstructionPython = function () {
-      const {inputCipheredTextVariable, inputSubstitutionVariable, outputSubstitutionVariable} = self.props.toolState;
+      const {inputCipheredTextVariable, inputSubstitutionVariable,
+             outputSubstitutionVariable, editable} = self.props.toolState;
+      const expr = (
+         <Python.Call name="analyseFrequenceBigrammes">
+            <Python.Var name={inputCipheredTextVariable}/>
+            <Python.Var name={inputSubstitutionVariable}/>
+            <span>…</span>
+         </Python.Call>
+      );
+      if (!editable)
+         return expr;
       return (
          <Python.Assign>
             <Python.Var name={outputSubstitutionVariable}/>
-            <Python.Call name="analyseFrequenceBigrammes">
-               <Python.Var name={inputCipheredTextVariable}/>
-               <Python.Var name={inputSubstitutionVariable}/>
-               <span>…</span>
-            </Python.Call>
+            {expr}
          </Python.Assign>
       );
    };
 
    const renderVariables = function () {
-      const {inputCipheredTextVariable, inputSubstitutionVariable, outputSubstitutionVariable} = self.props.toolState;
+      const {inputCipheredTextVariable, inputSubstitutionVariable,
+             outputSubstitutionVariable, editable} = self.props.toolState;
       const inputVars = [
          {label: "Texte chiffré analysé", name: inputCipheredTextVariable},
-         {label: "Substitution d'origine", name: inputSubstitutionVariable}
+         {
+            label: editable ? "Substitution d'origine" : "Substitution appliquée",
+            name: inputSubstitutionVariable
+         }
       ];
-      const outputVars = [
-         {label: "Nouvelle subsitution", name: outputSubstitutionVariable}
+      const outputVars = editable && [
+         {label: "Substitution proposée", name: outputSubstitutionVariable}
       ];
       return <Variables inputVars={inputVars} outputVars={outputVars} />;
-   };
-
-   const renderLock = function (cond) {
-      return cond ? <i className='fa fa-lock'></i> : ' ';
    };
 
    const renderCell = function (alphabet, cell) {
@@ -148,6 +154,7 @@ export const Component = PureComponent(self => {
 
    const renderFreqSubstBigrams = function (bigrams, inputSubstitution, outputSubstitution) {
       const {alphabet} = self.props.scope;
+      const {editable} = self.props.toolState;
       const {selectedPos} = self.state;
       const testConflict = function (cell1, cell2) {
          return cell1.q !== 'unknown' && cell2.q !== 'unknown' && cell1.l !== cell2.l;
@@ -162,12 +169,12 @@ export const Component = PureComponent(self => {
                <span className='originLetter'>
                   {renderCell(alphabet, inputCell)}
                </span>
-               <span className='newLetter'>
+               {editable && <span className='newLetter'>
                   {renderCell(alphabet, outputCell)}
-               </span>
-               <span className='substitutionLock'>
+               </span>}
+               {editable && <span className='substitutionLock'>
                   {isLocked ? <i className='fa fa-lock'></i> : ' '}
-               </span>
+               </span>}
             </div>
          );
       };
@@ -193,8 +200,10 @@ export const Component = PureComponent(self => {
             <div className='labels'>
                <div>Fréquences :</div>
                <div>Bigrammes :</div>
-               <div>Substitution d'origine :</div>
-               <div>Nouvelle substitution :</div>
+               {editable
+                  ? <div>Substitution d'origine :</div>
+                  : <div>Substitution :</div>}
+               {editable && <div>Nouvelle substitution :</div>}
             </div>
             {bigrams.map(renderFreqSubstBigram)}
          </div>
@@ -239,7 +248,7 @@ export const Component = PureComponent(self => {
 });
 
 export const compute = function (toolState, scope) {
-   const {substitutionEdits} = toolState;
+   const {substitutionEdits, compute} = toolState;
    const {alphabet, inputCipheredText, inputSubstitution} = scope;
    scope.textBigrams = getTextBigrams(inputCipheredText, alphabet);
    scope.mostFrequentBigrams = getMostFrequentBigrams(scope.textBigrams);
@@ -248,7 +257,7 @@ export const compute = function (toolState, scope) {
 
 export default self => {
    self.state = {
-  };
-  self.Component = Component;
-  self.compute = compute;
+   };
+   self.Component = Component;
+   self.compute = compute;
 };
