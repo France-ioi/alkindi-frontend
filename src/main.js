9f5f0f15defc54a4f955d2edfa7c423dddf0d4df
Sebastien Carlier <s.carlier@epixode.fr>
2016-01-09 16:02:15 +0000

    clean up

--- src/main.js
+++ src/main.js
@@ -32,7 +32,6 @@ window.onerror = function (message, url, line, column, error) {
       strError = err.toString();
       printer = 'null';
     }};
-    console.log(printer, strError);
     img.src = logUrl + '?' + [
       'version=' + encodeURIComponent(Alkindi.config.front_version),
       'user_id=' + encodeURIComponent(Alkindi.config.seed.user.id),
