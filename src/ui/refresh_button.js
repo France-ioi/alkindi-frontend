96d3984a2d5f24c134d96fd89e1f124a1c1d27cc
Sebastien Carlier <s.carlier@epixode.fr>
2016-01-10 17:56:54 +0000

    refresh button: support local refresh

--- src/ui/refresh_button.js
+++ src/ui/refresh_button.js
@@ -5,20 +5,29 @@ import {Button} from 'react-bootstrap';
 import {PureComponent} from '../misc';
 import classnames from 'classnames';
 
-export const selector = function (state) {
-  const {refreshing, refresh, user} = state;
-  return {refreshing, refresh, user};
+export const selector = function (state, props) {
+  const isPageRefresh = !('refresh' in props);
+  return {
+    refreshing: props.refreshing || state.refreshing,
+    refresh: props.refresh || state.refresh,
+    user: isPageRefresh && state.user,
+    isPageRefresh
+  };
 };
 
 export const RefreshButton = PureComponent(self => {
   const onClick = function () {
-    const {refresh, user} = self.props;
-    if (user === undefined)
-      return;
-    refresh(user.id);
+    const {isPageRefresh, refresh, user} = self.props;
+    if (isPageRefresh) {
+      if (user === undefined)
+        return;
+      refresh(user.id);
+    } else {
+      refresh();
+    }
   };
   self.render = function () {
-    const {refresh, refreshing} = self.props;
+    const refreshing = self.props.refreshing;
     const classes = refreshing ? ['fa','fa-spinner','fa-spin'] : ['fa','fa-refresh'];
     return (
       <Button bsStyle='primary' onClick={onClick} disabled={refreshing}>
