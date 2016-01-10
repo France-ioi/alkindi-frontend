a848b38fca0aaab8f03225359f5dc20487910e01
Sebastien Carlier <s.carlier@epixode.fr>
2016-01-10 23:15:48 +0000

    unstyled answers tab

--- src/tabs/answers.js
+++ src/tabs/answers.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {createSelector} from 'reselect';
+import Tooltip from '../ui/tooltip';
+import classnames from 'classnames';
 
 import {PureComponent} from '../misc';
-import {Answer} from '../playfair';
+import {AnswerDialog, Answer} from '../playfair';
 import Api from '../api';
 import Notifier from '../ui/notifier';
+import {RefreshButton} from '../ui/refresh_button';
 
 const AnswersTab = PureComponent(self => {
 
@@ -17,13 +20,96 @@ const AnswersTab = PureComponent(self => {
     api.submitAnswer(attempt.id, data);
   };
 
+  const onRefresh = function () {
+    const {attempt} = self.props;
+    self.setState({refreshing: true});
+    api.listAttemptAnswers(attempt.id).then(
+      function (result) {
+        self.setState({
+          refreshing: false,
+          answers: result.answers
+        });
+      },
+      function () {
+        self.setState({refreshing: false});
+      });
+  };
+
+  const renderAnswers = function (answers) {
+    const {submittedAnswerId} = self.props;
+    const renderAnswerRow = function (answer) {
+      const isCurrent = submittedAnswerId === answer.id;
+      const classes = [
+        isCurrent && 'answer-isSubmitted'
+      ];
+      return (
+        <tr key={answer.id} className={classnames(classes)}>
+          <td>
+            {isCurrent &&
+              <Tooltip content={<p>Vous venez de soumettre cette réponse.</p>}>
+                <i className="fa fa-asterisk"/>
+              </Tooltip>}
+          </td>
+          <td>{answer.ordinal}</td>
+          <td>{new Date(answer.created_at).toLocaleString()}</td>
+          <td><Answer answer={answer.answer}/></td>
+          <td>{Math.round(answer.score * 100)} %</td>
+          <td>
+            {answer.is_solution &&
+              <Tooltip content={<p>Cette réponse valide l'épreuve, félicitations !</p>}>
+                <i className="fa fa-thumbs-o-up"/>
+              </Tooltip>}
+          </td>
+        </tr>
+      );
+    };
+    return (
+      <table className="answer-list">
+        <thead>
+          <tr>
+            <th></th>
+            <th>#</th>
+            <th>Date de soumssion</th>
+            <th>Réponse</th>
+            <th>Score</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {answers.map(renderAnswerRow)}
+        </tbody>
+      </table>
+    );
+  };
+
+  self.componentWillMount = function () {
+    onRefresh();
+  };
+
   self.render = function () {
+    const {answers, refreshing} = self.state;
     return (
       <div className="wrapper">
+        <div style={{marginBottom: '10px'}}>
+          <div className='pull-right'>
+            <Tooltip content={<p>Cliquez sur ce bouton pour mettre à jour la liste des réponses soumises par votre équipe.</p>}/>
+            {' '}
+            <RefreshButton refresh={onRefresh} refreshing={refreshing}/>
+          </div>
+        </div>
         {notifier}
-        <Answer submit={submitAnswer}/>
+        <AnswerDialog submit={submitAnswer}/>
+        {answers === undefined
+          ? <p>Chargement en cours...</p>
+          : renderAnswers(answers)}
       </div>
     );
+  };
+}, _self => {
+  return {
+    refreshing: true,
+    answers: undefined,
+    submittedAnswerId: undefined
   };
 });
 
