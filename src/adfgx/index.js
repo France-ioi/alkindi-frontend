f10ae05f7ec300d792d2eadf8f2a4fa3708de9d6
Sebastien Carlier <s.carlier@epixode.fr>
2016-01-21 20:22:38 +0000

    whitespace

--- src/adfgx/index.js
+++ src/adfgx/index.js
@@ -18,76 +18,76 @@ const bigramAlphabet = bigram.makeAlphabet('ADFGX');
 
 export const setupTools = function (workspace) {
 
-  const iTextInput = workspace.addTool(TextInput, function (scopes, scope) {
-    // Set up the input scope for the tool's compute function.
-    // Each scope inherits prototypically from the root scope.
-    scope.text = scope.cipheredText;
-  }, {
-    outputTextVariable: "texteChiffré"
-  });
+   const iTextInput = workspace.addTool(TextInput, function (scopes, scope) {
+      // Set up the input scope for the tool's compute function.
+      // Each scope inherits prototypically from the root scope.
+      scope.text = scope.cipheredText;
+   }, {
+      outputTextVariable: "texteChiffré"
+   });
 
-  const iHints = workspace.addTool(Hints, function (scopes, scope) {
-    // scope.hintsGrid
-  }, {
-    outputGridVariable: "lettresGrilleIndices",
-    outputPermutationVariable: "permutationIndices"
-  });
+   const iHints = workspace.addTool(Hints, function (scopes, scope) {
+      // scope.hintsGrid
+   }, {
+      outputGridVariable: "lettresGrilleIndices",
+      outputPermutationVariable: "permutationIndices"
+   });
 
-  const iEnumeratePermutations = workspace.addTool(EnumeratePermutations, function (scopes, scope) {
-    scope.cipheredText = scopes[iTextInput].outputText;
-    scope.permutation = scopes[iHints].outputPermutation;
-  }, {
-    selected: '136245',
-    permutationData: {
-      '316245': {comment: 'à tester', favorited: true}
-    },
-    showOnlyFavorited: false,
-    inputPermutationVariable: 'permutationIndices',
-    inputCipheredText: 'texteChiffré',
-    outputPermutationVariable: 'permutationCourante'
-  });
+   const iEnumeratePermutations = workspace.addTool(EnumeratePermutations, function (scopes, scope) {
+      scope.cipheredText = scopes[iTextInput].outputText;
+      scope.permutation = scopes[iHints].outputPermutation;
+   }, {
+      selected: '136245',
+      permutationData: {
+         '316245': {comment: 'à tester', favorited: true}
+      },
+      showOnlyFavorited: false,
+      inputPermutationVariable: 'permutationIndices',
+      inputCipheredText: 'texteChiffré',
+      outputPermutationVariable: 'permutationCourante'
+   });
 
-  const iApplyPermutation = workspace.addTool(ApplyPermutation, function (scopes, scope) {
-    scope.cipheredText = scopes[iTextInput].outputText;
-    scope.permutation = scopes[iEnumeratePermutations].outputPermutation;
-  }, {
-    outputTextVariable: 'texteAprèsPermutation'
-  });
+   const iApplyPermutation = workspace.addTool(ApplyPermutation, function (scopes, scope) {
+      scope.cipheredText = scopes[iTextInput].outputText;
+      scope.permutation = scopes[iEnumeratePermutations].outputPermutation;
+   }, {
+      outputTextVariable: 'texteAprèsPermutation'
+   });
 
-  const iFrequencyAnalysis = workspace.addTool(FrequencyAnalysis, function (scopes, scope) {
-    scope.cipheredText = scopes[iApplyPermutation].outputText;
-    scope.permutation = scopes[iEnumeratePermutations].outputPermutation;
-  }, {
-    substitution: {}, // ex. {'AD': 'E'}
-    inputTextVariable: 'texteAprèsPermutation',
-    inputPermutation: 'permutationCourante',
-    outputSubstitutionVariable: 'substitutionÉditée'
-  });
+   const iFrequencyAnalysis = workspace.addTool(FrequencyAnalysis, function (scopes, scope) {
+      scope.cipheredText = scopes[iApplyPermutation].outputText;
+      scope.permutation = scopes[iEnumeratePermutations].outputPermutation;
+   }, {
+      substitution: {}, // ex. {'AD': 'E'}
+      inputTextVariable: 'texteAprèsPermutation',
+      inputPermutation: 'permutationCourante',
+      outputSubstitutionVariable: 'substitutionÉditée'
+   });
 
-  const iApplySubstitution = workspace.addTool(ApplySubstitution, function (scopes, scope) {
-    scope.cipheredText = scopes[iApplyPermutation].outputText;
-    scope.substitution = scopes[iFrequencyAnalysis].outputSubstitution;
-  }, {
-    inputTextVariable: 'texteAprèsPermutation',
-    inputSubstitutionVariable: 'substitutionÉditée',
-    outputClearTextVariable: 'texteDéchiffré'
-  });
+   const iApplySubstitution = workspace.addTool(ApplySubstitution, function (scopes, scope) {
+      scope.cipheredText = scopes[iApplyPermutation].outputText;
+      scope.substitution = scopes[iFrequencyAnalysis].outputSubstitution;
+   }, {
+      inputTextVariable: 'texteAprèsPermutation',
+      inputSubstitutionVariable: 'substitutionÉditée',
+      outputClearTextVariable: 'texteDéchiffré'
+   });
 
 };
 
 export const TabContent = PureComponent(self => {
 
-  self.render = function () {
-    const {workspace, task} = self.props;
-    console.log('task', task);
-    const rootScope = {
-      ...task,
-      clearAlphabet,
-      bigramAlphabet,
-      cipheredText: task.cipher_text,
-      hintsGrid: task.hints
-    };
-    return workspace.render(rootScope);
-  };
+   self.render = function () {
+      const {workspace, task} = self.props;
+      console.log('task', task);
+      const rootScope = {
+         ...task,
+         clearAlphabet,
+         bigramAlphabet,
+         cipheredText: task.cipher_text,
+         hintsGrid: task.hints
+      };
+      return workspace.render(rootScope);
+   };
 
 });
