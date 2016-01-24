f92c3210779177466e067ac8d3d98c9e55f8ba24
Sebastien Carlier <s.carlier@epixode.fr>
2016-01-24 16:45:38 +0000

    make is_training_open apply to all rounds

--- src/ui/attempt_timeline.js
+++ src/ui/attempt_timeline.js
@@ -171,9 +171,28 @@ export default PureComponent(self => {
     );
   };
 
+  const renderAttemptNotOpen = function (attempt) {
+    const {round} = self.props;
+    return (
+      <div>
+        {renderTimeline('unlock')}
+        <div className="timelineInfo">
+          <div className="timelineInfoContent">
+            <p>Votre équipe a confirmé l'accès au sujet, vous êtes
+               prêts à commencer dès l'ouverture de l'épreuve.
+            </p>
+            <Alert bsStyle='success'>
+              L'accès au sujet sera ouvert le {new Date(round.training_opens_at).toLocaleString()}.
+            </Alert>
+            {renderCancelAttempt(attempt)}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   const renderAttemptNotStarted = function (attempt) {
     const {round, team, user_id} = self.props;
-    const isOpen = !attempt.is_training || round.is_training_open;
     const showOwnAccessCode = !team.members.find(function (member) {
       return member.access_code && member.user_id === user_id;
     });
@@ -185,11 +204,8 @@ export default PureComponent(self => {
             {attempt.is_training
               ? (showOwnAccessCode
                  ? renderLateCodeEntry()
-                 : (round.is_training_open
-                      ? <p>Votre équipe a confirmé l'accès au sujet, vous pouvez
-                           maintenant le dévérouiller en cliquant.</p>
-                      : <p>Votre équipe a confirmé l'accès au sujet, vous êtes
-                           prêts à commencer dès l'ouverture de l'épreuve.</p>))
+                 : <p>Votre équipe a confirmé l'accès au sujet, vous pouvez
+                      maintenant le dévérouiller en cliquant.</p>)
               : <p>
                   Les membres de votre équipe ont donné leur accord pour accéder au sujet,
                   vous pouvez maintenant le déverouiller en cliquant.
@@ -199,15 +215,11 @@ export default PureComponent(self => {
                 <strong>Attention</strong>, après avoir accédé au sujet vous ne pourrez
                 plus changer la composition de votre équipe pendant le reste du concours.
               </p>}
-            {isOpen
-              ? <p className="text-center">
-                  <Button bsStyle="primary" bsSize="large" onClick={onAccessTask}>
-                    accéder au sujet <i className="fa fa-arrow-right"/>
-                  </Button>
-                </p>
-              : <Alert bsStyle='success'>
-                  L'accès au sujet sera ouvert le {new Date(round.training_opens_at).toLocaleString()}.
-                </Alert>}
+            <p className="text-center">
+              <Button bsStyle="primary" bsSize="large" onClick={onAccessTask}>
+                accéder au sujet <i className="fa fa-arrow-right"/>
+              </Button>
+            </p>
             {renderCancelAttempt(attempt)}
           </div>
         </div>
@@ -331,6 +343,8 @@ export default PureComponent(self => {
       return renderAttemptNotCreated(attempt);
     if (!attempt.started_at && attempt.needs_codes)
       return renderAttemptNotConfirmed(attempt);
+    if (!attempt.is_training_open) // XXX rename is_open
+      return renderAttemptNotOpen(attempt);
     if (!attempt.has_task)
       return renderAttemptNotStarted(attempt);
     if (!attempt.is_closed && !attempt.is_completed)
