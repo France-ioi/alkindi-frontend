6bed14979a15f7f4eb00293731eba1d2a157250f
Sebastien Carlier <s.carlier@epixode.fr>
2016-12-14 10:55:23 +0000

    set up index.html, bundle.js for production

--- src/index.js
+++ src/index.js
@@ -29,61 +29,58 @@ import {configure as configureAssets} from './assets';
 import {Api, ApiFactory} from './api'; // XXX
 import {WorkspaceManager} from './workspace';
 
-export default function (config, container) {
+const {store, scope, start} = link(function* (deps) {
 
-  const {store, scope, start} = link(function* (deps) {
+  yield include(App);
+  yield include(Login);
 
-    yield include(App);
-    yield include(Login);
-
-    yield defineAction('init', 'Init');
-    yield addReducer('init', function (state, action) {
-      return {
-        config,
-        response: {}
-      };
-    });
-
-    // XXX to refresh.js
-    yield defineAction('refresh', 'Refresh');
-
-    yield defineAction('setCsrfToken', 'SetCsrfToken');
-
-    yield defineSelector('getLoginUrl', function (state) {
-      return state.config.login_url;
-    });
-
-    yield defineSelector('getLogoutUrl', function (state) {
-      return state.config.logout_url;
-    });
-
+  yield defineAction('init', 'Init');
+  yield addReducer('init', function (state, action) {
+    const {config} = action;
+    return {
+      config,
+      response: {}
+    };
   });
 
+  // XXX to refresh.js
+  yield defineAction('refresh', 'Refresh');
+
+  yield defineAction('setCsrfToken', 'SetCsrfToken');
+
+  yield defineSelector('getLoginUrl', function (state) {
+    return state.config.login_url;
+  });
+
+  yield defineSelector('getLogoutUrl', function (state) {
+    return state.config.logout_url;
+  });
+
+});
+
+export function run (config, container) {
+
+  // Initialize the store.
   if ('assets_template' in config) {
-    configureAssets({template: config.assets_template});
+    configureAssets({template: config.assets_template}); // XXX
   }
-  store.dispatch({type: scope.init});
+  store.dispatch({type: scope.init, config});
   // const manager = WorkspaceManager(store); XXX
+
+  // Start the sagas.
   start();
   if ('seed' in config) {
     store.dispatch({type: 'END_REFRESH', response: config.seed});
   }
+
+  // Trigger an initial refresh.
   store.dispatch({type: scope.refresh});
+
+  // Render the application.
   // TODO: push DragDropContext into the task tab.
   const WrappedApp = DragDropContext(HTML5Backend)(scope.App);
   ReactDOM.render(<Provider store={store}><WrappedApp/></Provider>, container);
 
-  // This is the application's public interface.  FOR EXTERNAL USE ONLY!
-  const Alkindi = window.Alkindi = {};
-  Alkindi.config = config;
-  Alkindi.api = Api(config);
-  Alkindi.dispatch = function (action) {
-    store.dispatch(action);
-  };
-  Alkindi.logout = function () {
-    store.dispatch({type: scope.logout});
-  };
-  Alkindi.manager = WorkspaceManager(store);
+  return {store, scope};
 
-  return Alkindi;
 };
