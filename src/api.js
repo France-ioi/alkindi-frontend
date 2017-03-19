995face424a7adbc40e17f878005c94bbcc05699
Sebastien Carlier <s.carlier@epixode.fr>
2017-03-19 22:51:47 +0000

    update for backend_url config; add loginWithParticipationCode

--- src/api.js
+++ src/api.js
@@ -6,27 +6,29 @@ export const ApiFactory = function (methods) {
   const {get, post} = methods;
   return {
 
-    refresh: (request) => post('refresh', request),
+    refresh: (request) => post('api/refresh', request),
 
-    addBadge: (user_id, data) => post('users/'+user_id+'/add_badge', data),
+    addBadge: (user_id, data) => post('api/users/'+user_id+'/add_badge', data),
 
-    createTeam: (user_id) => post('users/'+user_id+'/create_team'),
-    joinTeam: (user_id, data) => post('users/'+user_id+'/join_team', data),
-    leaveTeam: (user_id) => post('users/'+user_id+'/leave_team'),
-    updateUserTeam: (user_id, data) => post('users/'+user_id+'/update_team', data),
+    createTeam: (user_id) => post('api/users/'+user_id+'/create_team'),
+    joinTeam: (user_id, data) => post('api/users/'+user_id+'/join_team', data),
+    leaveTeam: (user_id) => post('api/users/'+user_id+'/leave_team'),
+    updateUserTeam: (user_id, data) => post('api/users/'+user_id+'/update_team', data),
 
     createAttempt: (participation_id, round_task_id) => post('participations/'+participation_id+'/tasks/'+round_task_id+'/create_attempt'),
 
     // enterAccessCode: (user_id, data) => post('users/'+user_id+'/access_code', data),
-    startAttempt: (attempt_id) => post('attempts/'+attempt_id+'/start'),
+    startAttempt: (attempt_id) => post('api/attempts/'+attempt_id+'/start'),
 
-    getHint: (attempt_id, data) => post('attempts/'+attempt_id+'/get_hint', data),
-    resetHints: (attempt_id) => post('attempts/'+attempt_id+'/reset_hints'),
+    getHint: (attempt_id, data) => post('api/attempts/'+attempt_id+'/get_hint', data),
+    resetHints: (attempt_id) => post('api/attempts/'+attempt_id+'/reset_hints'),
 
-    submitAnswer: (user_id, attempt_id, data) => post(`users/${user_id}/attempts/${attempt_id}/answer`, data),
-    storeRevision: (user_id, attempt_id, data) => post(`users/${user_id}/attempts/${attempt_id}/store_revision`, data),
+    submitAnswer: (user_id, attempt_id, data) => post(`api/users/${user_id}/attempts/${attempt_id}/answer`, data),
+    storeRevision: (user_id, attempt_id, data) => post(`api/users/${user_id}/attempts/${attempt_id}/store_revision`, data),
 
-    loadRevision: (revision_id) => get(`workspace_revisions/${revision_id}`)
+    loadRevision: (revision_id) => get(`api/workspace_revisions/${revision_id}`),
+
+    loginWithParticipationCode: (data) => post('login/participation', data)
 
   };
 };
@@ -35,7 +37,7 @@ export const Api = function (config) {
 
   function get (path) {
     return new Promise(function (resolve, reject) {
-      var req = request.get(config.api_url + path);
+      var req = request.get(config.backend_url + path);
       req.end(function (err, res) {
         if (err || !res.ok)
           return reject({err, res});
@@ -46,7 +48,7 @@ export const Api = function (config) {
 
   function post (path, body) {
     return new Promise(function (resolve, reject) {
-      var req = request.post(config.api_url + path);
+      var req = request.post(config.backend_url + path);
       req.set('X-CSRF-Token', config.csrf_token);
       if (body) {
         req.set('Accept', 'application/json');
